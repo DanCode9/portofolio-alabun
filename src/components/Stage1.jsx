0d8e978eb9a8b76ff8af4b5da9f5aa705f1c131b
Dan <andreibarosan99@yahoo.com>
2024-04-20 21:06:37 +0200

    done3

--- src/components/Stage1.jsx
+++ src/components/Stage1.jsx
@@ -4,6 +4,9 @@ import img2 from "../img-stages/IMG_6869.png";
 import img3 from "../img-stages/IMG_6873.png";
 import img4 from "../img-stages/IMG_6877.png";
 import img5 from "../img-stages/IMG_7131.png";
+import img6 from "../img-stages/est1.jpg";
+import img7 from "../img-stages/est2.jpg";
+
 const Stage1 = () => {
   return (
     <div className="text-slate-200">
@@ -24,10 +27,11 @@ const Stage1 = () => {
         <p className="text-3xl ">Boîtes étiquetées</p>
         <img className="w-[50%]" src={img4} alt="" />
         <p className="text-3xl ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
-          cumque?
+          Système de gestion de tickets appelé service now
         </p>
-        <img className="w-[50%]" src={img5} alt="" />
+        <img className="w-[50%]" src={img6} alt="" />
+        <p className="text-3xl ">Fiche pour faire un prêt de matériel</p>
+        <img className="w-[50%]" src={img7} alt="" />
       </div>
     </div>
   );
